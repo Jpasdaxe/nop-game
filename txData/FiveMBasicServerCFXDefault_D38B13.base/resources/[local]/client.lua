7c8ccadf19df01311303dba83e812e39ca0e48df
Jpasdaxe <quentinvetu76520@gmail.com>
2026-04-18 10:46:18 +0200

    clean commit sans node_modules

--- txData/FiveMBasicServerCFXDefault_D38B13.base/resources/[local]/client.lua
+++
@@ -1,7 +0,0 @@
-Citizen.CreateThread(function()
-    while true do
-        Wait(0)
-        NetworkSetFriendlyFireOption(true)
-        SetCanAttackFriendly(PlayerPedId(), true, false)
-    end
-end)
